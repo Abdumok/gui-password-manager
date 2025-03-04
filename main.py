a10766e144564416f02c7efc642fa0e535791a48
Abdullahi Mohammmed Ali <abdumok19@gmail.com>
2025-03-04 23:27:57 +0300

    initial commit, setup main screen

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+# setup main screen
+window= Tk()
+window.title("Password Manager")
+window.minsize(width=600, height=400)
+window.config(padx=20, pady=20)
+
+
+
+
+
+
+window.mainloop()
