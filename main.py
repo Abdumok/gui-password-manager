f4852256932208c7b06d7af949a6f9d5dfe9cf70
Abdullahi Mohammmed Ali <abdumok19@gmail.com>
2025-03-05 23:35:12 +0300

    add buttons to the app screen

--- main.py
+++ main.py
@@ -22,4 +22,12 @@ email_input.place(x= 160, y= 230)
 password_input= Entry(width=27)
 password_input.place(x= 160, y=260)
 
+#========================================== Button ===================================================================
+gen_password_button= Button(width=18, text= "Generate Password")
+gen_password_button.place(x=329, y= 258)
+add_button= Button(width= 43, text= "Add")
+add_button.place(x= 156, y=289)
+search_button= Button(width=15, text="Search")
+search_button.place(x= 350, y= 197)
+
 window.mainloop()
