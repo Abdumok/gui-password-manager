0f569397070141ffee32ef15cded20f2a0c10875
Abdullahi Mohammmed Ali <abdumok19@gmail.com>
2025-03-05 23:08:43 +0300

    add entry fields

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ window.title("Password Manager")
 window.minsize(width=600, height=400)
 window.config(padx=20, pady=20)
 
-
+#=========================================== Labels ===================================================================
 website_text = Label(text="Website: ", font=("Times New Roman", 10, "bold"))
 website_text.place(x= 95, y= 200)
 email_text= Label(text= "Email/Username: ", font=("Times New Roman", 10, "bold"))
@@ -14,7 +14,12 @@ email_text.place(x= 50, y= 230)
 password_text= Label(text= "Password: ", font=("Times New Roman", 10, "bold"))
 password_text.place(x= 87, y= 260)
 
-
-
+#========================================== Entry ====================================================================
+website_input= Entry(width=30)
+website_input.place(x= 160, y= 200)
+email_input= Entry(width=50)
+email_input.place(x= 160, y= 230)
+password_input= Entry(width=27)
+password_input.place(x= 160, y=260)
 
 window.mainloop()
